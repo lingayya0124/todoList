e22191dcae77f39ee29032f4f1ad3c1c4df78aeb
lingayya0124 <lingayyah38@gmail.com>
2021-02-17 12:17:06 +0530

    changed to vue Version 2

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,65 +1,79 @@
 <template>
-    <div class="signup">
-       <h1 class="signup_heading">Sign Up</h1>
-       <form @submit.prevent="Signup">
-           <div class="textbox">
-                  <i class="fas fa-user"></i>
-       <input type="text" placeholder="Email" v-model="email"/>
-           </div>
-           <br>
-       <div class="textbox">
-          <i class="fas fa-lock"></i>
-       <input type="password" placeholder="Password" v-model="password"/>
-       </div>
-       <br>
-       <input class="submit_bt" type="submit" value="Signup">
-       <p>Have an Account? <router-link class="link" to="/login">Login Here</router-link></p>
-
-       </form>
-    </div>
+  <div class="signup">
+    <h1 class="signup_heading">Sign Up</h1>
+    <form @submit.prevent="Signup">
+      <div class="textbox">
+        <i class="fas fa-user"></i>
+        <input type="text" placeholder="Email" v-model="email" />
+      </div>
+      <br />
+      <div class="textbox">
+        <i class="fas fa-lock"></i>
+        <input type="password" placeholder="Password" v-model="password" />
+      </div>
+      <br />
+      <input
+        class="submit_bt"
+        v-on:click="signup"
+        type="submit"
+        value="Signup"
+      />
+      <p>
+        Have an Account?
+        <router-link class="link" to="/login">Login Here</router-link>
+      </p>
+    </form>
+  </div>
 </template>
 
 <script>
-import {ref} from 'vue';
-import firebase from 'firebase';
+import router from "vue-router";
+import firebase from "firebase";
 export default {
-     setup(){
-        const email = ref("");
-        const password = ref("");
-        
-        const Signup = () =>{
-            firebase
-            .auth()
-            .createUserWithEmailAndPassword(email.value,password.value)
-         
-            .catch(err => alert("Wrong Credential"));
-        }
-        return{
-            Signup,email,password
-}
-     }
-}
+  data: function () {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    signup: function (e) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(
+          (user) => {
+            this.$router.push("/");
+          },
+          (err) => {
+            alert(err.message);
+          }
+        );
+      e.preventDefault();
+    },
+  },
+};
 </script>
      
 <style scoped>
 @import "https://use.fontawesome.com/releases/v5.5.0/css/all.css";
 
-.signup{
+.signup {
   width: 280px;
   position: absolute;
   top: 50%;
   left: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
   color: white;
 }
- .signup_heading{
+.signup_heading {
   float: left;
   font-size: 40px;
   border-bottom: 6px solid #4caf50;
   margin-bottom: 50px;
   padding: 13px 0;
 }
-.textbox{
+.textbox {
   width: 100%;
   overflow: hidden;
   font-size: 20px;
@@ -67,12 +81,12 @@ export default {
   margin: 8px 0;
   border-bottom: 1px solid #4caf50;
 }
-.textbox i{
+.textbox i {
   width: 26px;
   float: left;
   text-align: center;
 }
-.textbox input{
+.textbox input {
   border: none;
   outline: none;
   background: none;
@@ -82,7 +96,7 @@ export default {
   float: left;
   margin: 0 10px;
 }
-.submit_bt{
+.submit_bt {
   width: 100%;
   background: none;
   border: 2px solid #4caf50;
@@ -91,8 +105,8 @@ export default {
   font-size: 18px;
   cursor: pointer;
   margin: 12px 0;
-} 
-.link{
+}
+.link {
   color: #4caf50;
 }
 </style>
